d486a48c68bcd666d46aeb3c316feabf3811e525
sofffja <sofi.efron@gmail.com>
2025-03-13 19:03:19 +0000

    Create intro placeholder component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,7 @@
+export default function Intro() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-white/95 font-[Nimbus-Bold] text-6xl text-[#f479e1] uppercase">
+      <h1>Clara Bunge</h1>
+    </div>
+  );
+}
