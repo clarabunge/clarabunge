f0bd8baede8878a63b83a99e5b581ad7f90e3c41
astrosuka <yegresvoselok@gmail.com>
2025-03-14 15:35:34 -0300

    Implement Project component with dynamic data fetching and video player integration

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,46 +1,76 @@
-export default function Project() {
-  return (
-    <div className="px-4">
-      <div className="py-16">
-        <VideoPlayer />
-      </div>
-      <h2 className="font-[Nimbus-Bold] text-4xl">YA PASO - Gregorio</h2>
-      <div className="pt-2 font-[Mplus] text-xs">
-        <div>Music Video</div>
-        <div>2024</div>
-      </div>
-      <div className="py-8">
-        “Ya Paso” delves into themes of self-discovery, passion, and the passage
-        of time. The story follows a young man at a career crossroads, torn
-        between the stability of a corporate job and the childhood passion that
-        still burns within him. As he wrestles with ambition and instinct, he
-        must choose between following convention or embracing the path his heart
-        truly desires, roller hockey.
-      </div>
-      <div className="text-end font-[Mplus] text-xs">
-        <div>Buenos Aires, Argentina</div>
-        <div>{`34°36′14″S 58°22′54″O`}</div>
-      </div>
-    </div>
-  );
-}
+import { useState } from "react";
+import { useMainContent } from "../utils/useData";
+import VideoPlayer from "./VideoPlayer";
+import ImageGallery from "./ImageGallery";
+import { PortableText } from "@portabletext/react";
+import useLanguage from "../utils/useLanguage";
 
-function VideoPlayer() {
+const SLUG = "ya-paso-gregorio";
+
+export default function Project() {
+  const [playerIsOpen, setPlayerIsOpen] = useState(false);
+  const { data, isLoading } = useMainContent();
+  const { language } = useLanguage();
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  const projectData = data?.projects?.find(
+    (project) => project.slug.current === SLUG,
+  );
+
   return (
-    <div style={{ padding: "56.25% 0 0 0" }} className="relative">
-      <iframe
-        src="https://player.vimeo.com/video/1047919336?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;transparent=0"
-        frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-        }}
-        title="IMG_6023"
-      ></iframe>
-    </div>
+    <>
+      {playerIsOpen && (
+        <div className="fixed top-0 left-0 z-100 flex h-screen w-full items-center justify-center bg-black">
+          <button
+            className="absolute top-4 right-4 flex size-8 cursor-pointer items-center justify-center rounded-full bg-black p-2 text-white"
+            onClick={() => setPlayerIsOpen(false)}
+          >
+            close
+          </button>
+          <VideoPlayer videoUrl={projectData?.videoUrl} />
+        </div>
+      )}
+      <section className="px-4">
+        <div
+          className="cursor-pointer py-16"
+          onClick={() => setPlayerIsOpen(true)}
+        >
+          <img src={projectData?.image?.url + "?h=1080&fm=webp"} alt="" />
+        </div>
+        <h2 className="font-[Nimbus-Bold] text-4xl">{projectData?.title}</h2>
+        <div className="pt-2 font-[Mplus] text-xs">
+          <div>{projectData?.typeOfProject?.type?.[language]}</div>
+          <div>{projectData?.date?.split("-")[0]}</div>
+        </div>
+        <div className="py-8">
+          <PortableText value={projectData?.description?.[language]} />
+        </div>
+        <div className="text-end font-[Mplus] text-xs">
+          <div>
+            {projectData?.location?.city}, {projectData?.location?.country}
+          </div>
+          <div>{projectData?.location?.coordinates}</div>
+        </div>
+        {projectData?.links && (
+          <ul className="py-8">
+            {projectData.links.map((link) => (
+              <li key={link._key}>
+                <a href={link.url} target="_blank">
+                  {link.title[language]}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+        <ImageGallery slug={projectData?.slug?.current} />
+      </section>
+    </>
   );
 }
