0c0b4793cb6b3a49d3ea1057c827e5b02dc14f2a
astrosuka <yegresvoselok@gmail.com>
2025-03-13 21:53:00 -0300

    Add React Query integration with QueryClientProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,24 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { LanguageProvider } from "./utils/LanguageContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 30,
+      cacheTime: 1000 * 60 * 35,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <LanguageProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </LanguageProvider>
     </BrowserRouter>
   </StrictMode>,
