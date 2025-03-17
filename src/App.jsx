1cd382a013ac2c93ee64790fbecfb5390c3d2ce4
astrosuka <yegresvoselok@gmail.com>
2025-03-17 16:06:26 -0300

    Add Info toggle functionality to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router";
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import Project from "./components/Project.jsx";
+import Info from "./components/Info.jsx";
 
 function App() {
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
   return (
     <>
-      <Header />
+      <Header infoIsOpen={infoIsOpen} setInfoIsOpen={setInfoIsOpen} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
       </Routes>
+      {infoIsOpen && <Info />}
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,31 @@
 import { NavLink } from "react-router";
 import LanguageButton from "./LanguageButton.jsx";
+import useLanguage from "../utils/useLanguage.js";
 
-export default function Header() {
+export default function Header({ infoIsOpen, setInfoIsOpen }) {
+  const { language } = useLanguage();
+  const close = {
+    en: "close",
+    es: "cerrar",
+  };
+
   return (
     <header className="fixed inset-0 z-20 flex h-min w-full items-start justify-between px-4 py-2 font-[Nimbus-Cond] uppercase mix-blend-difference">
-      <NavLink to="/">
+      <NavLink to="/" onClick={() => setInfoIsOpen(false)}>
         {/* <h1 className="font-[Nimbus-Bold] text-4xl">Clara Bunge</h1> */}
         <img
           src="/img/shapes.svg"
           alt="Clara Bunge"
-          className="-ml-6 w-80 rotate-12"
+          className="-ml-6 w-58 rotate-12"
         />
       </NavLink>
       <nav className="flex gap-2">
-        <a className="">?</a>
+        <button
+          className="cursor-pointer"
+          onClick={() => setInfoIsOpen((prev) => !prev)}
+        >
+          {infoIsOpen ? close[language] : "info"}
+        </button>
         <LanguageButton />
       </nav>
     </header>
