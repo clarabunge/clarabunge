e2589c3ae25f55049969791921f445759e315c27
astrosuka <yegresvoselok@gmail.com>
2025-03-18 14:34:11 -0300

    Add loading and error handling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import Project from "./components/Project.jsx";
 import Info from "./components/Info.jsx";
+import { useMainContent } from "./utils/useData.js";
 
 function App() {
   const [infoIsOpen, setInfoIsOpen] = useState(false);
+  const { isLoading, error } = useMainContent();
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <p>...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center">
+        <p>＞﹏＜</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Header infoIsOpen={infoIsOpen} setInfoIsOpen={setInfoIsOpen} />
