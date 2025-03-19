4bcfa8b4c62cfd8098891c04b928bbbd04f2459e
sofffja <sofi.efron@gmail.com>
2025-03-19 19:37:34 +0000

    Enhance Info component layout and styling; improve contact information display

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,35 +8,50 @@ export default function Info() {
 
   return (
     <div className="fixed top-0 left-0 z-10 flex h-screen w-full flex-col items-center justify-center font-[Nimbus-Cond] text-2xl backdrop-brightness-20 backdrop-grayscale-100">
-      <div className="max-w-3/4 columns-2 gap-8">
+      <div className="relative max-w-3/4 columns-2 gap-8">
         <div className="flex flex-col gap-4">
           <PortableText value={data?.about?.bio[language]} />
         </div>
-        {data?.about?.contact && (
-          <div className="my-8 text-xl text-white/70 uppercase">
-            <a href={`mailto:${data?.about?.contact.email}`}>
+
+        <div className="mt-8 flex flex-col items-start text-lg text-[var(--secondary)] uppercase">
+          {data?.about?.contact.email && (
+            <a
+              href={`mailto:${data?.about?.contact.email}`}
+              className="transition-colors hover:text-white"
+            >
               {data.about.contact.email}
             </a>
-            {data.about.contact.phone && <div>{data.about.contact.phone}</div>}
-            {data.about.contact.instagram && (
-              <a
-                href={`https://instagram.com/${data.about.contact.instagram}`}
-                target="_blank"
-              >
-                instagram
-              </a>
-            )}
-          </div>
-        )}
-        {data?.about?.cv?.asset?.url && (
-          <a
-            href={data?.about?.cv?.asset?.url}
-            target="_blank"
-            className="text-xl text-white/70 underline"
-          >
-            CV
-          </a>
-        )}
+          )}
+          {data.about.contact.phone && (
+            <div className="font-[display] transition-colors hover:text-white">
+              {data.about.contact.phone}
+            </div>
+          )}
+          {data.about.contact.instagram && (
+            <a
+              href={`https://instagram.com/${data.about.contact.instagram}`}
+              target="_blank"
+              className="transition-colors hover:text-white"
+            >
+              instagram
+            </a>
+          )}
+          {data?.about?.cv?.asset?.url && (
+            <a
+              href={data?.about?.cv?.asset?.url}
+              target="_blank"
+              className="text-lg text-[var(--secondary)] transition-colors hover:text-white"
+            >
+              CV
+            </a>
+          )}
+        </div>
+
+        <img
+          src="/img/corazon.svg"
+          alt=""
+          className="absolute right-1/4 -bottom-10 size-16 rotate-[20deg] opacity-50"
+        />
       </div>
       {/* {data?.about?.image?.url && (
         <img
