1b45fdc1e68f64e46d057fff9d1d22708d74b9dc
astrosuka <yegresvoselok@gmail.com>
2025-03-19 14:23:58 -0300

    Fix typo

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -6,7 +6,7 @@ export default function VideoPlayer({ videoUrl }) {
     <div className="relative pt-[56.25%]">
       <iframe
         src={`https://player.vimeo.com/video/${vimeoId}?badge=0&autopause=0&player_id=0&app_id=58479&transparent=1&autoplay=1&loop=1`}
-        frameBrder="0"
+        frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
         className="absolute top-0 left-0 h-full w-full"
       ></iframe>
