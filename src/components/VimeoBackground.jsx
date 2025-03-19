9af1c44fad1fd68e22c61cdf7a0d9c09bcb52bab
sofffja <sofi.efron@gmail.com>
2025-03-19 17:38:07 +0000

    Update Vimeo iframe source to include quality and loop parameters

--- src/components/VimeoBackground.jsx
+++ src/components/VimeoBackground.jsx
@@ -46,7 +46,7 @@ export default function VimeoBackground({
       >
         <motion.iframe
           ref={iframeRef}
-          src={`https://player.vimeo.com/video/${vimeoId}?badge=0&autopause=0&player_id=0&app_id=58479&background=1&autoplay=1`}
+          src={`https://player.vimeo.com/video/${vimeoId}?badge=0&autopause=0&player_id=0&app_id=58479&background=1&autoplay=1&quality=1080p&loop=1`}
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
           style={{
