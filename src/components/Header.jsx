6c96c48bbdeebd230b5136105bfa09271cbdf18b
astrosuka <yegresvoselok@gmail.com>
2025-03-25 19:51:45 -0300

    Fix header making underlying elements unclickable issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,15 +11,17 @@ export default function Header({ infoIsOpen, setInfoIsOpen }) {
   };
 
   return (
-    <header className="fixed inset-0 z-20 flex h-min w-full items-start justify-between p-4 text-sm mix-blend-difference select-none md:p-8">
-      <NavLink to="/" onClick={() => setInfoIsOpen(false)}>
-        <img
-          src="/img/clara1.svg"
-          alt="Clara Bunge"
-          className="-mt-2 -ml-2 w-42 invert hover:blur-[1px]"
-        />
-      </NavLink>
-      <nav className="flex gap-2 leading-none">
+    <header className="pointer-events-none fixed inset-0 z-20 flex h-min w-full items-start justify-between p-4 text-sm mix-blend-difference select-none md:p-8">
+      <div className="pointer-events-auto">
+        <NavLink to="/" onClick={() => setInfoIsOpen(false)}>
+          <img
+            src="/img/clara1.svg"
+            alt="Clara Bunge"
+            className="-mt-2 -ml-2 w-42 invert hover:blur-[1px]"
+          />
+        </NavLink>
+      </div>
+      <nav className="pointer-events-auto flex gap-2 leading-none">
         <button
           className={`group hover:bg-text hover:text-background cursor-pointer uppercase transition-all ${
             location.pathname === "/" || location.pathname === "/info"
