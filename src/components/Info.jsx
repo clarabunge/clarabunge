6dd371e8b7b3f8309aa4881c358776e388c8b5d5
astrosuka <yegresvoselok@gmail.com>
2025-03-27 13:27:05 -0300

    Add motion effects to Info component's SVG <3

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -62,7 +62,9 @@ export default function Info() {
           )}
         </div>
 
-        <svg
+        <motion.svg
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.5, transition: { delay: 0.5, duration: 0.65 } }}
           className="absolute right-8 size-16 opacity-50 select-none md:right-1/6 md:-bottom-6"
           fill="var(--color-primary)"
           id="Layer_2"
@@ -73,7 +75,7 @@ export default function Info() {
           <g id="Layer_1-2" data-name="Layer 1">
             <path d="M39.54,58.55c12.77-9.44,25.85-31.55,44.66-25.68,2.85.89,3.91,3.02,2.53,5.78L13.54,138.49c3.43-19.94-5.98-39.64-9.38-59.59-3.69-21.64-7.12-45.53-.07-66.78,5.64-16.98,17.76-14.87,26.33-1.47,8.58,13.42,6.59,32.67,9.11,47.88ZM29.54,74.54c7.24-15.45,6.88-53.51-4.92-66.05C14.5-2.26,9.81,3.4,6.52,14.54c-10.82,36.67,6.45,75.79,11.03,111.97L83.5,36.58c-15.79-2.08-33.11,14.65-43.08,25.87-3.68,4.14-3.33,10.76-10.88,12.09Z" />
           </g>
-        </svg>
+        </motion.svg>
       </div>
     </motion.div>
   );
